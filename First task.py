fb83363e2261266c29a57b73988be3dac3e43b3e
Nikolay <ncbm1q@gmail.com>
2022-11-05 13:46:52 +0300

    1

--- First task.py
+++ First task.py
@@ -10,7 +10,8 @@
 # Так же я решил добавить остальные месяцы, ибо мне писать это все понравилось)
 # Позже, погда все это написал, посмотрел твое решение и оказалось, что я усложнил себе задачу.
 
-print ("1 — январь, 2 — февраль, 3 — март,\n4 — апрель, 5 — май, 6 — июнь,\n7 — июль, 8 — август, 9 — сентябрь,\n10 — октябрь, 11 — ноябрь, 12 — декабрь\nВведите порядковый номер месяца:")
+print(
+    "1 — январь, 2 — февраль, 3 — март,\n4 — апрель, 5 — май, 6 — июнь,\n7 — июль, 8 — август, 9 — сентябрь,\n10 — октябрь, 11 — ноябрь, 12 — декабрь\nВведите порядковый номер месяца:")
 
 day_in_months = [1, 31, 2, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -24,7 +25,7 @@ month = day_in_months[months_numder_function]
 
 while months_numder_function == 2:
     print("Интересующий вас год высокосный? Ответьте, пожалуйста, словами <Да> или <Нет>.")
-    leap_year = str(input())
+    leap_year = input()
     if (leap_year == "Да") or (leap_year == "да"):
         month = 29
         months_numder_function = 3
@@ -35,7 +36,7 @@ while months_numder_function == 2:
         print("Некорректный ввод. Попробуйте, пожалуйста, еще раз.")
 
 total_working_time = month * 12
-print ("Общее рабочее время этом месяце составляет", total_working_time, "ч.")
+print("Общее рабочее время этом месяце составляет", total_working_time, "ч.")
 first_worker = 0
 second_worker = 0
 third_worker = 0
@@ -44,49 +45,38 @@ fourth_worker = 0
 while total_working_time > 0:
     if total_working_time < 1:
         break
-    else:
-        total_working_time = total_working_time - 12
-        first_worker = first_worker + 12
-        if total_working_time < 1:
-            break
-        else:
-            total_working_time = total_working_time - 12
-            first_worker = first_worker + 12
-            if total_working_time < 1:
-                break
-            else:
-                total_working_time = total_working_time - 12
-                second_worker = second_worker + 12
-                if total_working_time < 1:
-                    break
-                else:
-                    total_working_time = total_working_time - 12
-                    second_worker = second_worker + 12
-                    if total_working_time < 1:
-                        break
-                    else:
-                        total_working_time = total_working_time - 12
-                        third_worker = third_worker + 12
-                        if total_working_time < 1:
-                            break
-                        else:
-                            total_working_time = total_working_time - 12
-                            third_worker = third_worker + 12
-                            if total_working_time < 1:
-                                break
-                            else:
-                                total_working_time = total_working_time - 12
-                                fourth_worker = fourth_worker + 12
-                                if total_working_time < 1:
-                                    break
-                                else:
-                                    total_working_time = total_working_time - 12
-                                    fourth_worker = fourth_worker + 12
-
+    total_working_time = total_working_time - 12
+    first_worker = first_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    first_worker = first_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    second_worker = second_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    second_worker = second_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    third_worker = third_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    third_worker = third_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    fourth_worker = fourth_worker + 12
+    if total_working_time < 1:
+        break
+    total_working_time = total_working_time - 12
+    fourth_worker = fourth_worker + 12
 
 print("Первый рабочий отработал в этом месяце", first_worker, "ч.")
 print("Второй рабочий отработал в этом месяце", second_worker, "ч.")
 print("Третий рабочий отработал в этом месяце", third_worker, "ч.")
 print("Четвертый рабочий отработал в этом месяце", fourth_worker, "ч.")
-
-
